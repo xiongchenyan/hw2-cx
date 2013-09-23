f9fead107d9b7c126c188ff41ce3b37ce6e75581
xiongchenyan <xiongchenyan@gmail.com>
2013-09-23 14:35:49 -0400

    a

---
+++ src/main/java/edu/cmu/cs/lti/cx/annotators/Tokenization.java
@@ -0,0 +1,34 @@
+package edu.cmu.cs.lti.cx.annotators;
+
+
+import java.util.Properties;
+import edu.cmu.deiis.types.Token;
+import edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetEndAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
+public class Tokenization extends JCasAnnotator_ImplBase {
+
+	public Tokenization() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void process(JCas arg0) throws AnalysisEngineProcessException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
